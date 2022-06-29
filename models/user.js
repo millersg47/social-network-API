b85ea6e369312587241cdee60eb25fdc9e69987b
Sophie Miller <millersg47@gmail.com>
2022-06-28 17:48:57 -0700

    initial folder setup and userSchema file setup

---
+++ models/user.js
@@ -0,0 +1,37 @@
+const { Schema, Types } = require("mongoose");
+
+const userSchema = new Schema(
+  {
+    //not sure I need this userID field
+    userId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      //add email validation matching
+    },
+    thoughts: [{ type: Schema.Types.ObjectId, ref: "thought" }],
+    friends: [{ type: Schema.Types.ObjectId, ref: "user" }],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+userSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
+
+module.exports = assignmentSchema;
